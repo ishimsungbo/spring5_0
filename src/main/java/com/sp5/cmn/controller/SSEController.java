e1d9226f92d78e95d573b32ad683b3e34c5863fd
sungbo <ishimsungbo@gmail.com>
2023-06-25 11:18:59 +0900

    connect 스타일로해서 sse를 구현 해봅시다

--- src/main/java/com/sp5/cmn/controller/SSEController.java
+++ src/main/java/com/sp5/cmn/controller/SSEController.java
@@ -2,6 +2,7 @@ package com.sp5.cmn.controller;
 
 
 import com.sp5.cmn.service.MyEmitService;
+import com.sp5.cmn.service.SseEmitters;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
@@ -12,6 +13,8 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyEmitter;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
+import java.io.IOException;
+
 @RestController
 @Slf4j
 public class SSEController {
@@ -20,11 +23,6 @@ public class SSEController {
     private MyEmitService myEmitService;
 
     @GetMapping(value = "/sseTest", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
-    public String sseTest() {
-        return "123";
-    }
-
-    @GetMapping(value = "/sseTest2", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     public ResponseBodyEmitter sseTest2() {
         log.debug("SSe Call debug =================================");
 
@@ -36,12 +34,18 @@ public class SSEController {
         return emitter;
     }
 
-    @GetMapping(value = "/server")
-    public String server(){
-        log.debug("debug =================================");
-        log.debug("안녕하세요. debug");
-        log.info("안녕하세요. info");
-        return "HI MY NAME IS SERVER EXAMPLE-안녕하세요";
-    }
+    /*
+    @GetMapping(value = "/sseTest3", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public ResponseEntity<SseEmitter> sseTest3() {
+        log.debug("SSe Call debug =================================");
 
+        SseEmitter emitter = new SseEmitter();
+        myEmitService.add(emitter);
+
+        log.debug("실행했습니다. ResponseBodyEmitter sseTest2");
+
+        return ResponseEntity.ok(emitter);
+    }*/
+
+    //TODO 프론트에 지속적으로 전달하는 TEXT_EVENT_STREAM_VALUE 를 만든다.
 }
--- src/main/java/com/sp5/cmn/controller/SseController2.java
+++ src/main/java/com/sp5/cmn/controller/SseController2.java
@@ -40,4 +40,5 @@ public class SseController2 {
         }
         return ResponseEntity.ok(emitter);
     }
+
 }
